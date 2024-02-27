f16184ff4395ec208dca3b5641e9d1887709a50a
Nour Muhammed <nourmuhammed20121994@gmail.com>
2024-02-27 09:36:39 +0200

    olderd

--- resources/views/website/layout/master.blade.php
+++ resources/views/website/layout/master.blade.php
@@ -33,21 +33,18 @@
     <link rel="stylesheet" href="{{asset('site/splide/dist/css/splide.min.css')}}"/>
     <link rel="stylesheet" href="{{asset('site/css/home.css')}}"/>
     <script src="{{asset('site/splide/dist/js/splide.min.js')}}"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jscroll/2.4.1/jquery.jscroll.min.js"></script>
+{{--    <script src="https://cdnjs.cloudflare.com/ajax/libs/jscroll/2.4.1/jquery.jscroll.min.js"></script>--}}
     <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
+{{--    <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">--}}
+
     <style>
         .active_categery {
             border: 3px solid var(--main_color);
             border-radius: 8px;
         }
     </style>
-    <script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
-
-
 </head>
 <body>
-{!! \Brian2694\Toastr\Facades\Toastr::message() !!}
-
 <div class="mycontainer">
 @include('website.layout.header')
 
@@ -100,6 +97,9 @@
         });
     });
 </script>
+<script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
+
+{!! Toastr::message() !!}
 <script src="{{asset('site/js/bootstrap.bundle.js')}}"></script>
 <script src="{{asset('site/js/cart.js')}}"></script>
 <script src="{{asset('site/assets/js/swiper-bundle.min.js')}}"></script>
